f9683afb55ee6c1e2fd7b61946915cab1cbb20ef
Illia Medvid-Hnepa <illyagnepa1@gmail.com>
2024-08-08 21:25:39 +0300

    Added comments

--- Task_3/Task_3.py
+++ Task_3/Task_3.py
@@ -2,6 +2,7 @@ import sys
 from colorama import Fore
 from pathlib import Path
 
+# Main cycle, which recursively shows all files and folders. Added colors to separate them.
 def print_tree(path, prefix=''):
     for item in path.iterdir():
         if item.is_dir():
@@ -10,9 +11,13 @@ def print_tree(path, prefix=''):
             print(f'{prefix}{Fore.BLUE}├── {item.name}{Fore.RESET}')
         if item.is_dir():
             print_tree(item, prefix + Fore.GREEN + '│   ' + Fore.RESET)
+
+# Empty input check
 try:
+    # Check if required path exists
     if Path(sys.argv[1]).exists()  == False:
         print("Path doen't exist")
+    # Check if directory provided as an input
     elif Path(sys.argv[1]).is_dir() == False:
         print("Please provide a directory")
     else:
